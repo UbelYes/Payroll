9fc8751f4cac0f8df26c25c952194f5b0a5c9dee
Alfredo Julienne Sarmiento <2242297@slu.edu.ph>
2025-06-22 23:11:43 +0800

    CodeBase commit

---
+++ src/CodeBase/Payroll.java
@@ -0,0 +1,45 @@
+package CodeBase;
+
+public class Payroll {
+    private String employeeId;
+    private String employeeName;
+    private String department;
+    private double basicSalary;
+    private double overtimeHours;
+    private double hourlyRate;
+    private double overtimeRate;
+    private double overtimePay;
+    private double grossPay;
+    private double sssContribution;
+    private double philHealthContribution;
+    private double pagIbigContribution;
+    private double incomeTax;
+    private double totalDeductions;
+    private double netPay;
+
+    //default constructor
+    // Initializes all fields to default values
+    public Payroll() {
+        this.employeeId = "";
+        this.employeeName = "";
+        this.department = "";
+        this.basicSalary = 0.0;
+        this.overtimeHours = 0.0;
+        this.hourlyRate = 0.0;
+        this.overtimeRate = 0.0;
+        this.overtimePay = 0.0;
+        this.grossPay = 0.0;
+        this.sssContribution = 0.0;
+        this.philHealthContribution = 0.0;
+        this.pagIbigContribution = 0.0;
+        this.incomeTax = 0.0;
+        this.totalDeductions = 0.0;
+        this.netPay = 0.0;
+    }
+
+    //setters and getters
+
+    /**
+     * Complete the code
+     */
+}
---
+++ src/CodeBase/StatutoryDeductions.java
@@ -0,0 +1,29 @@
+package CodeBase;
+
+public class StatutoryDeductions {
+
+    public double calculateSSSContribution() {
+
+    }
+
+    public double calculatePhilHealthContribution() {
+        /*if (basicSalary <= 10000){
+            return 250
+        } else if (basicSalary >= 100000){
+            return 2500;
+        } else if (basicSalary >= 50000){
+            return 1250;
+        else {
+            // Linear interpolation between ₱10,000 and ₱50,000
+            return 250 + ((basicSalary - 10000) / 40000) * 1000;
+        }*/
+    }
+    
+    public double calculatePagIbigContribution() {
+        return ;
+    }
+
+    public double calculateIncomeTax(double grossPay) {
+        return;
+    }
+}
